17e5d6fdf9c05457d56aeabfe64684e40ede2088
sky603629 <ding_chengjie@hoperun.com>
2026-09-15 16:46:27 +0800

    chore: remove Android and iOS related files
    Signed-off-by: sky603629 <ding_chengjie@hoperun.com>

--- android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughTouchHandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package com.rome2rio.android.reactnativetouchthroughview;
-
-import android.view.MotionEvent;
-
-public interface TouchThroughTouchHandlerInterface {
-    public TouchThroughTouchHandler getTouchThroughTouchHandler();
-}
